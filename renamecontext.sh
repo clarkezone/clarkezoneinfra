fc71e4f93dba0bdeaac79173526b0474a9e4a1b6
James Clarke <james@clarkezone.net>
2022-08-12 18:08:50 +0000

    Fixtypo

--- renamecontext.sh
+++ renamecontext.sh
@@ -6,21 +6,31 @@ while getopts ":i:u:c:t:" option; do
         u) export clusteruser=$OPTARG;;
         t) export contextname=$OPTARG;;
 	\?) 
-	   echo "renamecontext -i <file> -c <clustername> -u <clusteruser> -n <contextname>"
+	   echo "renamecontext -i <file> -c <clustername> -u <clusteruser> -t <contextname>"
 	   exit;;
     esac
 done
+if [ -z "$file"  ]
+then
+echo "missing file renamecontext -i <file> -c <clustername> -u <clusteruser> -t <contextname>"
+exit
+fi
 if [ -z "$clustername"  ]
 then
-echo "missing clustername"
+echo "missing clustername renamecontext -i <file> -c <clustername> -u <clusteruser> -t <contextname>"
+exit
+fi
+if [ -z "$clusteruser"  ]
+then
+echo "missing cluster user renamecontext -i <file> -c <clustername> -u <clusteruser> -t <contextname>"
+exit
+fi
+if [ -z "$contextname"  ]
+then
+echo "missing contextname renamecontext -i <file> -c <clustername> -u <clusteruser> -t <contextname>"
 exit
 fi
 
-#export clustername="monitoringcluster-do"
-
-# export clustername="monitoringcluster-do"
-# export clusteruser="monitoringclusteruser"
-# export contextname="monitoringclusterctx"
 yq e -i '.clusters[0].name=env(clustername)' "$file"
 yq e -i '.users[0].name=env(clusteruser)' "$file"
 yq e -i '.contexts[0].context.cluster=env(clustername)' "$file"
